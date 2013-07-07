127ced8e2358226b01a57fb888edcadf0eb4305e
Conrad Lee <conradlee1985@gmail.com>
2013-07-07 21:30:18 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new Buffer(256);
-
-  response.send(buffer.toString(fs.readFileSync('index.html')));
+var mybuf = fs.readFileSync('index.html');
+var mybuf2 = mybuf.toString();
+  response.send(mybuf2);
 });
 
 var port = process.env.PORT || 5000;
