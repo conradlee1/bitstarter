2cb48d376e465ae216bcc11a2f02c2260df211d0
Conrad Lee <conradlee1985@gmail.com>
2013-07-08 20:32:32 +0000

    added grader.js

--- grader.js
+++ grader.js
@@ -22,10 +22,14 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "url.html";
+var DEFAULT_URL = "";
+var DEFAULT_CHECK = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +38,21 @@ var assertFileExists = function(infile) {
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var response2console = function(result, response) {
+    if (result instanceof Error) {
+	console.erroe('Error: ' + util.format(response.message));
+    } else {
+	fs.writeFileSync('url.html', result);
+	var checkJson = checkHtmlFile('url.html', DEFAULT_CHECK);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+};
+
+var checkUrl = function(infile) {
+    DEFAULT_URL = infile;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,10 +84,18 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_links>', 'Path to index.html', clone(checkUrl))
 	.parse(process.argv);
+    var argvs = JSON.stringify(process.argv, null, 4);
+    if (argvs.indexOf("-u") == -1){
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    }
+    else {
+    DEFAULT_CHECK = program.checks;
+    rest.get(DEFAULT_URL).on('complete', response2console);
+     }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
